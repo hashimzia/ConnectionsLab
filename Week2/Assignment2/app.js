aa1d7bce8503a6425cff81d1d5eed9ac54676144
hashimzia <108898519+hashimzia@users.noreply.github.com>
2022-09-13 23:05:11 +0400

    Add files via upload

--- Week2/Assignment2/app.js
+++ Week2/Assignment2/app.js
@@ -3,14 +3,15 @@ window.addEventListener('scroll', () => {
     let blue = 118 + ((window.scrollY / 1055) * 100)
     document.body.style.backgroundColor = `rgb(181,101,${blue})`
 })
+comments = [];
 
+h3 = document.querySelector('h3');
 btn = document.querySelector('button');
-btn.addEventListener('mouseover', () => {
-    console.dir(btn);
-    btn.textContent = 'Click me';
-    btn.style.letterSpacing = '2px';
-})
-btn.addEventListener('click', () => {
-
-    window.scrollTo(0, 0);
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    commentText = document.querySelector('textarea');
+    commentUser = document.querySelector('#user');
+    comment = document.createElement('p');
+    comment.innerText = commentText.value + " - " + commentUser.value;
+    h3.after(comment);
 })
