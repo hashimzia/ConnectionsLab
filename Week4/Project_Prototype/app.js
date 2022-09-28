2a4e0ade4bd2bcad242fafdc0753189a97daec2d
hashimzia <hz2776@nyu.edu>
2022-09-28 23:49:45 +0400

    creating end screen

--- Week4/Project_Prototype/app.js
+++ Week4/Project_Prototype/app.js
@@ -1,5 +1,6 @@
 let playButton = document.querySelector('button');
 let higher = document.querySelector("#higher");
+let score = 0;
 document.body.addEventListener('click', () => {
     fade = 0;
 })
@@ -10,7 +11,12 @@ setTimeout(() => {
 }, 100);
 
 playButton.addEventListener('click', () => {
+    debugger
+    document.querySelector("h2").classList.remove('hidden');
     canvas.classList.remove('hidden');
+    let scoreHTML = document.createElement('h2');
+    scoreHTML.innerHTML = `Score:${score}`;
+    document.querySelector('main').appendChild(scoreHTML);
     let main = document.querySelector('main');
     main.remove();
     document.querySelector("#part1").after(main);
@@ -19,8 +25,8 @@ playButton.addEventListener('click', () => {
     for (let div of divs) {
         div.classList.remove('hidden');
     }
-    let rand1 = Math.floor(Math.random() * 600);
-    let rand2 = Math.floor(Math.random() * 600);
+    let rand1 = Math.floor(Math.random() * 400);
+    let rand2 = Math.floor(Math.random() * 400);
     name1 = document.querySelector("#name1");
     name2 = document.querySelector("#name2");
     stat1 = document.querySelector("#stat1");
@@ -51,63 +57,65 @@ playButton.addEventListener('click', () => {
             img2.src = `/sprites/${data.name}.gif`
         })
     higher.addEventListener('click', async () => {
+        higher.classList.add('hidden');
+        lower.classList.add('hidden');
         let i = 0;
         for (i = 0; i < base_stat_total_2; i++) {
+
             stat2.innerHTML = i;
             await delay(3);
         }
+
+
         stat2.innerHTML = base_stat_total_2;
         if (base_stat_total_2 >= base_stat_total_1) {
             correct = true;
+            scoreHTML.innerHTML = `Score:${++score}`
             setTimeout(() => {
                 swap();
                 randomize();
             }, 1000)
+            higher.classList.remove('hidden');
+            lower.classList.remove('hidden');
         }
         else {
-            correct = false;
+            lost();
         }
     })
     lower.addEventListener('click', async () => {
+        higher.classList.add('hidden');
+        lower.classList.add('hidden');
         let i = 0;
         for (i = 0; i < base_stat_total_2; i++) {
             stat2.innerHTML = i;
             await delay(3);
         }
         stat2.innerHTML = base_stat_total_2;
+
+
         if (base_stat_total_2 <= base_stat_total_1) {
             correct = true;
+            scoreHTML.innerHTML = `Score:${++score}`;
             setTimeout(() => {
                 swap();
                 randomize();
             }, 1000)
+            higher.classList.remove('hidden');
+            lower.classList.remove('hidden');
         }
         else {
-            correct = false;
-
+            lost();
         }
     })
 })
 
 function randomize() {
-    let rand1 = Math.floor(Math.random() * 900);
-    let rand2 = Math.floor(Math.random() * 900);
+    let rand1 = Math.floor(Math.random() * 400);
+    let rand2 = Math.floor(Math.random() * 400);
     name1 = document.querySelector("#name1");
     name2 = document.querySelector("#name2");
     stat1 = document.querySelector("#stat1");
     compare = document.querySelector("#comparison");
-    // fetch(`https://pokeapi.co/api/v2/pokemon/${rand1}`)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         name1.innerHTML = data.name;
-    //         base_stat_total_1 = 0;
-    //         for (let stat of data.stats) {
-    //             base_stat_total_1 += stat.base_stat;
-    //         }
-    //         stat1.innerHTML = base_stat_total_1;
-    //         console.log(base_stat_total_1);
-    //         compare.innerHTML = `than the base stat total of ${data.name}`;
-    //     });
     fetch(`https://pokeapi.co/api/v2/pokemon/${rand2}`)
         .then((response) => response.json())
         .then((data) => {
@@ -130,4 +138,20 @@ function swap() {
     base_stat_total_1 = base_stat_total_2;
     img1.src = img2.src;
 
+}
+
+function lost() {
+    correct = false;
+    setTimeout(() => {
+        sides = document.querySelectorAll('.sides');
+        for (let side of sides) {
+            side.style.display = 'none';
+        }
+
+        let lostHTML = document.createElement('h1');
+        let main = document.querySelector('main');
+
+        lostHTML.innerHTML = 'You got it wrong!';
+        main.appendChild(lostHTML);
+    }, 1000)
 }
